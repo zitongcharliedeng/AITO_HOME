c5504375abd460b6bd332ad25b1cd99a722edac2
The Zilchotron6000 <zitongcharliedeng2003@gmail.com>
2025-12-22 22:57:01 +0000

    test: try QXL graphics instead of virtio-gpu for niri

--- nixos_system_config/approval_tests/FIRST_BOOT_SHOWS_LOGIN/default.nix
+++ nixos_system_config/approval_tests/FIRST_BOOT_SHOWS_LOGIN/default.nix
@@ -4,7 +4,7 @@ pkgs.testers.runNixOSTest {
   name = "FIRST_BOOT_SHOWS_LOGIN";
   nodes.machine = {
     imports = systemModules.TEST_VM;
-    virtualisation.qemu.options = [ "-vga none" "-device virtio-gpu-pci" ];
+    virtualisation.qemu.options = [ "-vga qxl" ];
     virtualisation.memorySize = 2048;
   };
   testScript = builtins.readFile ./default.py;
--- nixos_system_config/approval_tests/TERMINAL_IS_VISIBLE_ON_LEFT_HALF_OF_SCREEN_SHOWING_HOME_DIRECTORY_IS_A_GIT_REPO/default.nix
+++ nixos_system_config/approval_tests/TERMINAL_IS_VISIBLE_ON_LEFT_HALF_OF_SCREEN_SHOWING_HOME_DIRECTORY_IS_A_GIT_REPO/default.nix
@@ -4,7 +4,7 @@ pkgs.testers.runNixOSTest {
   name = "TERMINAL_IS_VISIBLE_ON_LEFT_HALF_OF_SCREEN_SHOWING_HOME_DIRECTORY_IS_A_GIT_REPO";
   nodes.machine = {
     imports = systemModules.TEST_VM;
-    virtualisation.qemu.options = [ "-vga none" "-device virtio-gpu-pci" ];
+    virtualisation.qemu.options = [ "-vga qxl" ];
     virtualisation.memorySize = 2048;
   };
   testScript = builtins.readFile ./default.py;
