3fcbb17e57cf45126c4c42c772a56085a1fe3256
The Zilchotron6000 <zitongcharliedeng2003@gmail.com>
2026-01-01 16:26:58 +0000

    fix: add auto-format for installer root device

--- nixos_system_config/approval_tests/SYSTEM_INSTALLS_FROM_ONE_SCRIPT/default.nix
+++ nixos_system_config/approval_tests/SYSTEM_INSTALLS_FROM_ONE_SCRIPT/default.nix
@@ -14,17 +14,36 @@
 # - installer node boots from /dev/vdb, installs to /dev/vda
 # - target node boots from /dev/vda (shares state_dir with installer)
 
+let
+  # Module to auto-format the installer's root device
+  autoFormatRootDevice = { lib, config, pkgs, ... }:
+    let rootDevice = config.virtualisation.rootDevice;
+    in {
+      boot.initrd.extraUtilsCommands = lib.mkIf (!config.boot.initrd.systemd.enable) ''
+        copy_bin_and_libs ${pkgs.e2fsprogs}/bin/mke2fs
+      '';
+      boot.initrd.postDeviceCommands = lib.mkIf (!config.boot.initrd.systemd.enable) ''
+        FSTYPE=$(blkid -o value -s TYPE ${rootDevice} || true)
+        PARTTYPE=$(blkid -o value -s PTTYPE ${rootDevice} || true)
+        if test -z "$FSTYPE" -a -z "$PARTTYPE"; then
+            mke2fs -t ext4 ${rootDevice}
+        fi
+      '';
+    };
+in
 pkgs.testers.runNixOSTest {
   name = "SYSTEM_INSTALLS_FROM_ONE_SCRIPT";
 
   nodes = {
     # The installer: simulates NixOS ISO environment
     installer = { config, pkgs, lib, ... }: {
+      imports = [ autoFormatRootDevice ];
+
       virtualisation = {
         memorySize = 4096;
         diskSize = 8192;
         cores = 4;
-        # Boot from small disk, install to main disk
+        # Boot from small disk (/dev/vdb), install to main disk (/dev/vda)
         emptyDiskImages = [ 512 ];
         rootDevice = "/dev/vdb";
       };
@@ -40,13 +59,6 @@ pkgs.testers.runNixOSTest {
 
       # Enable flakes
       nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-      # Include all dependencies needed for the install
-      # (test VM can't access network)
-      system.extraDependencies = with pkgs; [
-        stdenv
-        bintools
-      ];
 
       # Make the repo available (simulates git clone)
       virtualisation.sharedDirectories.repo = {
